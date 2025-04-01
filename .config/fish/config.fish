e69da374527e066f3c196ca0990467437be829b5
David <lummotm@proton.me>
2025-04-01 21:41:54 +0200

    fish fixes

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+end
+
+abbr vim nvim 
+abbr vi nvim 
+
+abbr vim nvim 
+abbr vi nvim 
+
+
