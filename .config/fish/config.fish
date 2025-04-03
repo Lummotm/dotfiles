f8a747dcc5f1545acfaf97ceb80cf6d8fbfc0899
David <lummotm@proton.me>
2025-04-04 01:47:36 +0200

    tmux last fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,8 @@ abbr vi nvim
 abbr gc "git commit -m"
 abbr gp "git push -u origin main"
 
+alias ll="eza -l --git --icons --color=auto --group-directories-first"
+alias ls="eza --icons -a --color=auto --group-directories-first"
 alias hotspot="/home/davidn/Projects/Bash/hotspot.sh"
 
 set -gx MANPAGER 'nvim +Man!'
