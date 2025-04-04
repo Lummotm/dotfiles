40d05bda5bd016233696ab0bddd574fd133019b9
David <lummotm@proton.me>
2025-04-04 04:15:56 +0200

    feat(nvim): add core options for clipboard and UI
    - Enable unnamedplus clipboard integration
    - Set tab/indentation to 2 spaces
    - Configure line numbers and cursor highlights

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,11 +1,53 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true
-vim.opt.cursorline = true
-vim.opt.tabstop = 2       -- Cantidad de espacios que un tab representa
-vim.opt.shiftwidth = 2    -- Cantidad de espacios para indentación automática
-vim.opt.softtabstop = 2   -- Cantidad de espacios al presionar Tab
-vim.opt.expandtab = true  -- Convierte tabs a espacios
-vim.opt.mouse = "" -- Desactivar mouse completamente
-vim.g.mapleader = " " -- Establece la tecla de leader como espacio
-vim.g.maplocalleader = " " 
+--[[
+  NEOVIM CORE CONFIGURATION
+  Minimalist yet powerful setup with thoughtful defaults
+--]]
+
+--------------------------------------------------------------------------------
+-- Visual Settings
+--------------------------------------------------------------------------------
+-- Line Numbers & Appearance
+vim.opt.number = true          -- Show absolute line numbers
+vim.opt.relativenumber = true  -- Relative numbers for easier navigation
+vim.opt.cursorline = true      -- Highlight current line
+vim.opt.termguicolors = true   -- Enable 24-bit RGB color support
+vim.opt.showmode = false       -- Hide mode (covered by statusline)
+
+-- Whitespace & Indentation
+vim.opt.tabstop = 2           -- Number of spaces a TAB represents
+vim.opt.shiftwidth = 2        -- Spaces for auto-indentation
+vim.opt.softtabstop = 2       -- Spaces inserted when pressing TAB
+vim.opt.expandtab = true      -- Convert tabs to spaces
+vim.opt.breakindent = true    -- Maintain indentation on wrapped lines
+
+--------------------------------------------------------------------------------
+-- Behavior & Performance
+--------------------------------------------------------------------------------
+-- File Handling
+vim.opt.undofile = true       -- Persistent undo history
+vim.opt.confirm = true        -- Confirm before unsaved changes
+
+-- Timing & Responsiveness
+vim.opt.updatetime = 250      -- Faster updates (for plugins)
+vim.opt.timeoutlen = 300      -- Timeout for key sequences
+
+-- UI Elements
+vim.opt.signcolumn = 'yes'    -- Always show sign column (for LSP/git)
+
+--------------------------------------------------------------------------------
+-- Input & Navigation
+--------------------------------------------------------------------------------
+-- Mouse & Keybindings
+vim.opt.mouse = ""            -- Disable mouse completely
+vim.g.mapleader = " "         -- Set space as leader key
+vim.g.maplocalleader = " "    -- Local leader for buffer-specific mappings
+
+-- Clipboard Integration
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'  -- System clipboard integration
+end)
+
+--[[
+  Note: Additional plugin configurations should be added
+  in their respective files under lua/plugins/
+--]]
