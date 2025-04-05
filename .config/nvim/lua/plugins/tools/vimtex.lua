d74c14f9b466dd0c72cb6077365717118da4674a
David <lummotm@proton.me>
2025-04-05 20:19:55 +0200

    fix(nvim): fixed vimtex delete after compilation

--- .config/nvim/lua/plugins/tools/vimtex.lua
+++ .config/nvim/lua/plugins/tools/vimtex.lua
@@ -2,7 +2,7 @@ return {
 	"lervag/vimtex",
 	lazy = false,
 	init = function()
-		-- Desactivar el módulo de sintaxis de vimtex (usa Treesitter)
+		-- Desactivar el módulo de sintaxis de vimtex (usamos Treesitter)
 		vim.g.vimtex_syntax_enabled = 0
 
 		-- Configurar el compilador
@@ -15,7 +15,7 @@ return {
 				"-file-line-error",
 				"-synctex=1",
 				"-interaction=nonstopmode",
-				"-c", -- Esto limpia los archivos auxiliares después de compilar
+				"-outdir=build",
 			},
 		}
 
@@ -26,14 +26,15 @@ return {
 		vim.g.vimtex_view_method = "zathura"
 	end,
 	config = function()
-		-- Sobrescribir el comando :VimtexCompile para usar vimux
+		-- Sobrescribir el comando :VimtexCompile para usar Vimux
 		vim.cmd([[
-    function! s:compile_with_vimux() abort
-    let l:cmd = 'latexmk -pdf -c -interaction=nonstopmode %' " Agregado -c aquí también
-    call VimuxRunCommand(l:cmd)
-    endfunction
+      function! s:compile_with_vimux() abort
+        let l:cmd = 'latexmk -pdf -interaction=nonstopmode -outdir=build % && latexmk -c -outdir=build %'
+        call VimuxRunCommand(l:cmd)
+      endfunction
 
-    command! VimtexCompile call s:compile_with_vimux()
+      command! VimtexCompile call s:compile_with_vimux()
     ]])
 	end,
 }
+
