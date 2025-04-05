c184604b67804339bd872ec6bc2fdfa5e7273d82
David <lummotm@proton.me>
2025-04-05 04:05:11 +0200

    feat(lsp): update LSP config and add null-ls integrations

---
+++ .config/nvim/lua/plugins/tools/completions.lua
@@ -0,0 +1,75 @@
+return {
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          -- ========== 🔥 Mapeos SuperTab (Solución 4) ==========
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump() -- Expande el snippet o salta al siguiente nodo
+            else
+              fallback() -- Comportamiento normal del Tab
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1) -- Salta al nodo anterior del snippet
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          -- ========== Mapeos originales (opcionales) ==========
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+
+      -- Configuración para la línea de comandos (se mantiene igual)
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+    end,
+  },
+}
--- .config/nvim/lua/plugins/tools/lsp-config.lua
+++ .config/nvim/lua/plugins/tools/lsp-config.lua
@@ -1,51 +1,58 @@
--- LSP o Language Server Protocol es un servicio que permite que el compilador 
+-- LSP o Language Server Protocol es un servicio que permite que el compilador
 -- que sabe como va el codigo y todo eso, sea capaz de tener influencia en
--- los cambios que hacemos reportando errores, etc. 
+-- los cambios que hacemos reportando errores, etc.
 
 return {
-  {
-    "williamboman/mason.nvim",
-    -- mason permite administrar servidores de lenguaje instalarlos etc.
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    -- mason-lspconfig sirve para integrar mason con lsp, es super util por el tag 
-    -- ensure_installed que permite que se haga de manera automatica la instalación
-    -- del servidor de lenguaje y todo eso 
-    config = function()
-      require("mason-lspconfig").setup{
-        ensure_installed = {
-          "lua_ls",
-          "matlab_ls",
-          "texlab",
-        }
-      }
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.matlab_ls.setup({})
-      lspconfig.texlab.setup({})
-      -- Configuración global de diagnósticos
-      vim.diagnostic.config({
-        virtual_text = {
-          prefix = "● ", -- Puedes cambiar esto por '■', '▎', etc.
-          source = "if_many",
-          format = function(diagnostic)
-            return string.format("%s", diagnostic.message)
-          end,
-        },
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-        severity_sort = true,
-      })
-    end
-  }
+	{
+		"williamboman/mason.nvim",
+		-- mason permite administrar servidores de lenguaje instalarlos etc.
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		-- mason-lspconfig sirve para integrar mason con lsp, es super util por el tag
+		-- ensure_installed que permite que se haga de manera automatica la instalación
+		-- del servidor de lenguaje y todo eso
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"matlab_ls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.matlab_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.texlab.setup({
+				capabilities = capabilities,
+			})
+			-- Configuración global de diagnósticos
+			vim.diagnostic.config({
+				virtual_text = {
+					prefix = "● ", -- Puedes cambiar esto por '■', '▎', etc.
+					source = "if_many",
+					format = function(diagnostic)
+						return string.format("%s", diagnostic.message)
+					end,
+				},
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
+		end,
+	},
 }
