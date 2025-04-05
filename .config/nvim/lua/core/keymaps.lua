4b46ac3c39745c7900fede8eb24bd3b5a45b217a
David <lummotm@proton.me>
2025-04-05 04:05:26 +0200

    chore(core): update keymaps and general options

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -5,25 +5,27 @@ local keymap = vim.keymap.set
 keymap("n", "<leader>w", ":w<CR>", { desc = "Guardar archivo" })
 
 -- 2. Keymaps de plugins que no requieran configuración importante
-keymap("n", "<Leader>ff", ":Telescope find_files<CR>", { desc = "Opens file search with telescope" })
 keymap("n", "<C-n>", ":Neotree toggle left<CR>")
+keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
+keymap('n', '<leader>fh', '<cmd>Telescope oldfiles<CR>')
+keymap('n', '<leader>fr', '<cmd>Telescope frecency<CR>')
+keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>')
 
 
 -- 3. Keymaps del LSP (nativos de Neovim)
 
 -- 4. Keymaps de plugins complejos (con carga diferida)
 local function setup_plugin_keymaps()
-  -- La idea de cargarlo así es la siguiente, si el plugin es complejo o requiere más control que simplemente dar un comando 
+  -- La idea de cargarlo así es la siguiente, si el plugin es complejo o requiere más control que simplemente dar un comando
   -- como es el caso en :NvimTreeToggle y infimo más rendimiento entonces es mejor usar la carga con la API.
   -- Aunque es verdad que es el estandar actual de neovim
 
-  local lspconfig = require("lspconfig")
   keymap("n", "K", vim.lsp.buf.hover, {})
   keymap("n", "gd", vim.lsp.buf.definition, {})
-  keymap({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+  keymap({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+
+  keymap("n", "<leader>gf", vim.lsp.buf.format, {})
 end
-
-
 
 -- Cargar después de que todo esté listo
 vim.api.nvim_create_autocmd("User", {
--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -7,46 +7,45 @@
 -- Visual Settings
 --------------------------------------------------------------------------------
 -- Line Numbers & Appearance
-vim.opt.number = true          -- Show absolute line numbers
-vim.opt.relativenumber = true  -- Relative numbers for easier navigation
-vim.opt.cursorline = true      -- Highlight current line
-vim.opt.termguicolors = true   -- Enable 24-bit RGB color support
-vim.opt.showmode = false       -- Hide mode (covered by statusline)
+vim.opt.number = true -- Show absolute line numbers
+vim.opt.relativenumber = true -- Relative numbers for easier navigation
+vim.opt.cursorline = true -- Highlight current line
+vim.opt.termguicolors = true -- Enable 24-bit RGB color support
+vim.opt.showmode = false -- Hide mode (covered by statusline)
 
 -- Whitespace & Indentation
-vim.opt.tabstop = 2           -- Number of spaces a TAB represents
-vim.opt.shiftwidth = 2        -- Spaces for auto-indentation
-vim.opt.softtabstop = 2       -- Spaces inserted when pressing TAB
-vim.opt.expandtab = true      -- Convert tabs to spaces
-vim.opt.breakindent = true    -- Maintain indentation on wrapped lines
+vim.opt.tabstop = 2 -- Number of spaces a TAB represents
+vim.opt.shiftwidth = 2 -- Spaces for auto-indentation
+vim.opt.softtabstop = 2 -- Spaces inserted when pressing TAB
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.breakindent = true -- Maintain indentation on wrapped lines
 
 --------------------------------------------------------------------------------
 -- Behavior & Performance
 --------------------------------------------------------------------------------
 -- File Handling
-vim.opt.undofile = true       -- Persistent undo history
-vim.opt.confirm = true        -- Confirm before unsaved changes
+vim.opt.undofile = true -- Persistent undo history
+vim.opt.confirm = true -- Confirm before unsaved changes
 
 -- Timing & Responsiveness
-vim.opt.updatetime = 250      -- Faster updates (for plugins)
-vim.opt.timeoutlen = 300      -- Timeout for key sequences
+vim.opt.updatetime = 250 -- Faster updates (for plugins)
+vim.opt.timeoutlen = 500 -- Timeout for key sequences
 
 -- UI Elements
-vim.opt.signcolumn = 'yes'    -- Always show sign column (for LSP/git)
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
+vim.opt.signcolumn = "yes" -- Always show sign column (for LSP/git)
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 --------------------------------------------------------------------------------
 -- Input & Navigation
 --------------------------------------------------------------------------------
 -- Mouse & Keybindings
-vim.opt.mouse = ""            -- Disable mouse completely
-vim.g.mapleader = " "         -- Set space as leader key
-vim.g.maplocalleader = " "    -- Local leader for buffer-specific mappings
+vim.opt.mouse = "" -- Disable mouse completely
+vim.g.mapleader = " " -- Set space as leader key
+vim.g.maplocalleader = " " -- Local leader for buffer-specific mappings
 
 -- Clipboard Integration
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'  -- System clipboard integration
+	vim.opt.clipboard = "unnamedplus" -- System clipboard integration
 end)
 
 --[[
