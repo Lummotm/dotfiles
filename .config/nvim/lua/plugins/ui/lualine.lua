666e734b36c7b998888808da98b9527db665af01
David <lummotm@proton.me>
2025-04-05 04:05:16 +0200

    style(ui): update theme (tokyonight) and lualine config

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -1,20 +1,20 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- Opcional: para íconos
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'auto', -- Puedes usar 'tokyonight', 'onedark', etc.
-        icons_enabled = true,
-      },
-      sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-      },
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- Opcional: para íconos
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "auto", -- Puedes usar 'tokyonight', 'onedark', etc.
+				icons_enabled = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
--- .config/nvim/lua/plugins/ui/tokyonight.lua
+++ .config/nvim/lua/plugins/ui/tokyonight.lua
@@ -1,22 +1,22 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    lazy = false,
-    config = function()
-      require("tokyonight").setup({
-        style = "night",
-        transparent = true,
-        styles = {
-          sidebars = "transparent",
-          floats = "transparent",
-        }
-      })
-      vim.cmd.colorscheme("tokyonight")
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+				transparent = true,
+				styles = {
+					sidebars = "transparent",
+					floats = "transparent",
+				},
+			})
+			vim.cmd.colorscheme("tokyonight")
 
-      -- Ajustes de transparencia
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
-    end
-  }
+			-- Ajustes de transparencia
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
+		end,
+	},
 }
