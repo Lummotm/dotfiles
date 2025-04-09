95f961956986f55a74f458a9f051ec4e2387cc31
David <lummotm@proton.me>
2025-04-09 11:53:57 +0200

    fish(feat): added ls after change of directory

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,8 @@ abbr vi nvim
 abbr gc "git commit -m"
 abbr gp "git push -u origin main"
 
-alias ll="eza -l --git --icons --color=auto --group-directories-first"
-alias ls="eza --icons --color=auto --group-directories-first"
+alias ll="eza -l --git --icons --color=auto --group-directories-first --sort=extension"
+alias ls="eza --icons --color=auto --group-directories-first --sort=extension"
 alias hotspot="/home/davidn/Projects/Bash/hotspot.sh"
 
 set -gx MANPAGER 'nvim +Man!'
