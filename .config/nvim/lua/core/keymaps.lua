3779fe01042be472960689fd8c447f352eef2176
Lummotm <lummotm@proton.me>
2025-05-01 02:34:05 +0200

    feat(nvim): added indent keybind

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,8 +1,9 @@
 local keymap = vim.keymap.set
 
-keymap({ "n" }, "<C-s>", ":w<CR>", { desc = "Save file" })
-keymap({ "n" }, "<C-q>", ":q<CR>", { desc = "Exist File" }) -- no need for save and exit due to confirmation when exit
-vim.api.nvim_set_keymap('n', '<C-A>', 'ggvg0G', { noremap = true, silent = true })
+keymap("n", "<C-s>", ":w<CR>", { desc = "Save file" })
+keymap("n", "<C-q>", ":q<CR>", { desc = "Exist File" }) -- no need for save and exit due to confirmation when exit
+keymap("n", "<C-A>", "ggvg0G", { noremap = true, silent = true })
+keymap("n", "<leader>gi", "magg=G`a", { desc = "Autoindent entire file and return to cursor" })
 
 keymap("n", "<C-n>", ":Neotree toggle left<CR>")
 
@@ -14,14 +15,14 @@ keymap("n", "<leader>ll", ":VimtexCompile<CR>", { desc = "Compile with latexmk",
 keymap("n", "<leader>lc", ":VimtexClean<CR>", { desc = "Clean complation files" })
 
 local function setup_plugin_keymaps()
-	keymap("n", "K", vim.lsp.buf.hover, { desc = "View Lsp Documentation" })
-	keymap("n", "gd", vim.lsp.buf.definition, { desc = "View Lsp Definition" })
-	keymap({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+  keymap("n", "K", vim.lsp.buf.hover, { desc = "View Lsp Documentation" })
+  keymap("n", "gd", vim.lsp.buf.definition, { desc = "View Lsp Definition" })
+  keymap({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
 
-	keymap("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format code" })
+  keymap("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format code" })
 end
 
 vim.api.nvim_create_autocmd("User", {
-	pattern = "VeryLazy",
-	callback = setup_plugin_keymaps,
+  pattern = "VeryLazy",
+  callback = setup_plugin_keymaps,
 })
