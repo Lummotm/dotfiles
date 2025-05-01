0d7b668801aa4e337b80e35dde476899d1940fda
Lummotm <lummotm@proton.me>
2025-05-01 16:13:03 +0200

    fix(vimtex): stoped warning messages on useless things

--- .config/nvim/lua/plugins/tools/vimtex.lua
+++ .config/nvim/lua/plugins/tools/vimtex.lua
@@ -5,6 +5,11 @@ return {
     vim.g.vimtex_syntax_enabled = 0
     vim.g.vimtex_compiler_method = "latexmk"
     vim.g.vimtex_view_method = 'zathura'
+    vim.g.vimtex_quickfix_ignore_filters = {  -- Ignora estos warnings
+      "Underfull",
+      "Overfull",
+      "specifier changed to",
+    }
     vim.g.vimtex_compiler_latexmk = {
       continuous = 1,
       options = {
