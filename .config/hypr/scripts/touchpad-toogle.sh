ff5c35ea3c6c36618e900bb7a73a43848fa1f360
Lummotm <lummotm@proton.me>
2025-05-09 17:49:42 +0200

    feat(hypr): added touchpad toggle on F8

---
+++ .config/hypr/scripts/touchpad-toogle.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DEVICE="asue120b:00-04f3:31c0-touchpad"
+VAR="device[$DEVICE]:enabled"
+FLAG="/tmp/hypr-touchpad-disabled"
+
+if [ -f "$FLAG" ]; then
+    hyprctl keyword "$VAR" true
+    rm "$FLAG"
+    notify-send "🖱 Touchpad activado"
+else
+    hyprctl keyword "$VAR" false
+    touch "$FLAG"
+    notify-send "🖱 Touchpad desactivado"
+fi
+
