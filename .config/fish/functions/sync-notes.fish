1d1d39d1623d2c531361f78b1c1108d6d37dd2c7
Lummotm <lummotm@proton.me>
2025-05-17 18:30:35 +0200

    fix(sync-notes): syncs empty folders too

--- .config/fish/functions/sync-notes.fish
+++ .config/fish/functions/sync-notes.fish
@@ -1,8 +1,13 @@
 function sync-notes
     cd ~/Documents/Obsidian/
+
+    # Añadir .gitkeep en carpetas vacías
+    find . -type d -empty -exec touch {}/.gitkeep \;
+
     git add .
     if not git diff --cached --quiet
-        git commit -m "Sync desde PC (date '+%Y-%m-%d %H:%M')"
+        set fecha (date '+%Y-%m-%d %H:%M')
+        git commit -m "Sync desde PC ($fecha)"
     end
     git pull --rebase
     git push
