031005a11a577f7661b310663791b1ac6cb5f327
Lummotm <lummotm@proton.me>
2025-05-24 17:33:29 +0200

    fix(hyprland,tmux): moved scripts to hyprland for a more logical order
    Added hotspot toggle script, toogle on hotspot if connected to ethernet
    on startup

---
+++ .config/hypr/scripts/start-hotspot-if-eth.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CARRIER_FILE="/sys/class/net/enp4s0f3u1c2/carrier"
+
+if [ -f "$CARRIER_FILE" ] && [ "$(cat "$CARRIER_FILE")" == "1" ]; then
+    nmcli device wifi hotspot ifname wlan0 ssid ThinkingRock band bg password ThinkingRock123 && nmcli dev wifi show-password
+else 
+    echo "Ethernet not connected"
+    exit 0
+fi
