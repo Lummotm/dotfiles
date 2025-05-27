3adf66ee59928ba2baf15473437a25d56a383cbb
Lummotm <lummotm@proton.me>
2025-05-27 14:33:03 +0200

    feat(rofi): created new script adding and searching notes

--- .config/rofi/launchers/launcher.sh
+++ .config/rofi/launchers/launcher.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 dir="$HOME/.config/rofi/launchers/style-launcher.rasi"
 music="$HOME/.config/rofi/scripts/music-picker.sh"
+notes="$HOME/.config/rofi/scripts/notes-picker.sh"
 rofi \
-    -modi "drun,music:$music,window" \
+    -modi "drun,music:$music,notes:$notes,window" \
     -show drun \
     -theme "$dir"
 
---
+++ .config/rofi/scripts/notes-picker.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+notepath="$HOME/Documents/Obsidian/"
+
+if [ -z "$ROFI_RETV" ] || [ "$ROFI_RETV" -eq 0 ]; then
+    {
+        echo "Nueva nota"
+        fd -e md . "$notepath" -p | while read -r path; do
+            basename "$path"
+        done
+    }
+    exit 0
+fi
+
+# Acción: recuperar la ruta completa
+
+selected="$1"
+[ -z "$selected" ] && exit 1
+
+if [ "$selected" = "Nueva nota" ]; then
+    targetdir="$HOME/Documents/Obsidian/001 - inbox/"
+    name="$(date +%F_%T | tr -d ':').md"
+    fullpath="$targetdir/$name"
+    setsid foot nvim "$fullpath" >/dev/null 2>&1 </dev/null &
+    exit 0
+else
+    fullpath=$(fd -e md . "$notepath" -p | grep "/$selected$")
+    setsid foot nvim "$fullpath" >/dev/null 2>&1 </dev/null &
+fi
