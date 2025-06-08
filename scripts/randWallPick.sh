91c4fbee4e12f72e0b8b8ba50e3c7878abfbeefc
Lummotm <lummotm@proton.me>
2025-06-08 19:40:09 +0200

    fixed extre ram usage

--- scripts/randWallPick.sh
+++ scripts/randWallPick.sh
@@ -4,10 +4,13 @@ base="$HOME/Pictures/Wallpapers/rand-wall"
 file=$(fd -e png -e jpg . "$HOME/Pictures/Wallpapers/wall-randomizer/" | shuf -n 1)
 swww img "$file" --transition-type random --transition-step 255 --transition-fps 60
 
+# Reshuffle for lockscreen
+file=$(fd -e png -e jpg . "$HOME/Pictures/Wallpapers/wall-randomizer/" | shuf -n 1)
+
 ext="${file##*.}"
 if [ "$ext" != "png" ]; then
     rm -f "$base.png"
-    ffmpeg -y -i "$file" "$base.png" >/dev/null 2>&1
+    ffmpeg -y -i "$file" "$base.png"
 else
     [ "$file" != "$base.png" ] && cp "$file" "$base.png"
 fi
