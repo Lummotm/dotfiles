e9e1ccd3fe5bc4340acc8f56796a280cf66046c3
Lummotm <lummotm@proton.me>
2025-06-09 02:18:12 +0200

    fixed not taking into account the remote files

--- scripts/sync-notes.sh
+++ scripts/sync-notes.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
+cd ~/Documents/Obsidian || exit
 
-cd ~/Documents/Obsidian/ || exit
-
+# Crear .gitkeep en directorios vacíos
 find . -type d -empty -exec touch {}/.gitkeep \;
 
+# Agregar todos los cambios
 git add -A
-# Comprobar si hay algo para comitear
+
+# Verificar si hay cambios remotos
+git fetch
+
+# Hacer commit si hay cambios locales
 if ! git diff --cached --quiet; then
     fecha=$(date '+%Y-%m-%d %H:%M')
     git commit -m "Sync desde PC $fecha"
-    git pull --rebase
-    git push
-else
-    echo "No hay cambios para sincronizar."
 fi
+
+# Sincronizar siempre (por si hay cambios remotos)
+git pull --rebase && git push
