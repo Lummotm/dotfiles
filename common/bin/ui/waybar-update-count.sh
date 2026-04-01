701c12993146e3d14032e7bb18b2c7d1ea6b2444
David Nikolov <Lummotm@proton.me>
2026-04-01 23:01:40 +0200

    (fix): quitamos los paquetes de haskell y python que en general son dependencia de otros paquetes y no requieren manteninmiento

--- common/bin/ui/waybar-update-count.sh
+++ common/bin/ui/waybar-update-count.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-OFFICIAL_UPDATES=$(checkupdates 2>/dev/null)
-AUR_UPDATES=$(yay -Qua 2>/dev/null)
+IGNORE_PATTERN="^(haskell-|python-)"
+
+OFFICIAL_UPDATES=$(checkupdates 2>/dev/null | grep -vE "$IGNORE_PATTERN")
+AUR_UPDATES=$(yay -Qua 2>/dev/null | grep -vE "$IGNORE_PATTERN")
 
 COUNT_OFFICIAL=$(echo "$OFFICIAL_UPDATES" | grep -c '[^[:space:]]')
 COUNT_AUR=$(echo "$AUR_UPDATES" | grep -c '[^[:space:]]')
@@ -9,11 +11,12 @@ TOTAL=$((COUNT_OFFICIAL + COUNT_AUR))
 
 HAS_KERNEL=$(echo "$OFFICIAL_UPDATES" | grep -iE "^linux(-cachyos| )" | wc -l)
 
-if [ "$HAS_KERNEL" -gt 0 ] || [ "$TOTAL" -ge 50 ]; then
+if [ "$TOTAL" -gt 50 ]; then
     ICON="󰮯"
     [[ "$HAS_KERNEL" -gt 0 ]] && ICON="⚠"
 
     echo "{\"text\": \"$ICON\", \"tooltip\": \"Paquetes: $TOTAL\nKernel detectado: $HAS_KERNEL\n\nClick para safe-update\"}"
 else
+    # Si después del filtro no queda nada, Waybar no muestra nada
     echo "{\"text\": \"\", \"tooltip\": \"\"}"
 fi
