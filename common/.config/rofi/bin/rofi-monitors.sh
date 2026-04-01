c1b82153211cdc49a28c20e227338747f57a83a0
David Nikolov <Lummotm@proton.me>
2026-04-01 20:52:54 +0200

    upgraded the script
    now supports both monitors

--- common/.config/rofi/bin/rofi-monitors.sh
+++ common/.config/rofi/bin/rofi-monitors.sh
@@ -2,24 +2,25 @@
 source "$HOME/.config/rofi/bin/dependencies/rofi-core.sh"
 
 INTERNAL="eDP-1"
-EXTERNAL="HDMI-A-1"
+EXT_HDMI="HDMI-A-1"
+EXT_DP="DP-1"
 STATE_FLAG="/tmp/monitor-off-flag"
 ROTATED_FLAG="/tmp/monitor-rotated-flag"
 
 LAPTOP_ONLY=" Only Laptop"
 EXT_ONLY="󰍹 Only External"
 ROTATE=" Swap to Vertical"
-TOGGLE_ON_OF="󰶐 Both Off"
+TOGGLE_ON_OF="󰶐 All Off"
 
 rofi_cmd() {
     rofi_core -w "21ch" -mesg "Select an option:" -N -c 'textbox{ padding: 5px;}'
 }
 
-if [ -f $ROTATED_FLAG ]; then
+if [ -f "$ROTATED_FLAG" ]; then
     ROTATE=" Swap to Horizontal"
 fi
-if [ -f $STATE_FLAG ]; then
-    TOGGLE_ON_OF="󰍺 Both On"
+if [ -f "$STATE_FLAG" ]; then
+    TOGGLE_ON_OF="󰍺 All On"
 fi
 
 if [[ -n "$(pgrep niri-session)" ]]; then
@@ -29,9 +30,9 @@ elif [[ -n "$(pgrep hyprland)" ]]; then
 fi
 
 monitor_toggle_cmd() {
-    monitor=$1
-    compositor=$2
-    state=$3
+    local monitor=$1
+    local compositor=$2
+    local state=$3
 
     if [[ "$compositor" == "niri" ]]; then
         niri msg output "$monitor" "$state"
@@ -54,27 +55,32 @@ monitor_rotate() {
 }
 
 options="$LAPTOP_ONLY\n$EXT_ONLY\n$TOGGLE_ON_OF\n$ROTATE"
-
 selected=$(echo -e "$options" | rofi_cmd)
 
 case "$selected" in
 "$LAPTOP_ONLY")
     monitor_toggle_cmd "$INTERNAL" "$COMPOSITOR" on
-    monitor_toggle_cmd "$EXTERNAL" "$COMPOSITOR" off
+    monitor_toggle_cmd "$EXT_HDMI" "$COMPOSITOR" off
+    monitor_toggle_cmd "$EXT_DP" "$COMPOSITOR" off
     rm -f "$STATE_FLAG"
     ;;
 "$EXT_ONLY")
-    monitor_toggle_cmd "$EXTERNAL" "$COMPOSITOR" on
+    monitor_toggle_cmd "$EXT_HDMI" "$COMPOSITOR" on
+    monitor_toggle_cmd "$EXT_DP" "$COMPOSITOR" on
     monitor_toggle_cmd "$INTERNAL" "$COMPOSITOR" off
     rm -f "$STATE_FLAG"
     ;;
 "$TOGGLE_ON_OF")
-    if [ -f $STATE_FLAG ]; then
-        monitor_toggle_cmd "$EXTERNAL" "$COMPOSITOR" on
+    if [ -f "$STATE_FLAG" ]; then
+        monitor_toggle_cmd "$EXT_HDMI" "$COMPOSITOR" on
+        monitor_toggle_cmd "$EXT_DP" "$COMPOSITOR" on
         monitor_toggle_cmd "$INTERNAL" "$COMPOSITOR" on
+        rm -f "$STATE_FLAG"
     else
-        monitor_toggle_cmd "$EXTERNAL" "$COMPOSITOR" off
+        monitor_toggle_cmd "$EXT_HDMI" "$COMPOSITOR" off
+        monitor_toggle_cmd "$EXT_DP" "$COMPOSITOR" off
         monitor_toggle_cmd "$INTERNAL" "$COMPOSITOR" off
+        touch "$STATE_FLAG"
     fi
     ;;
 "$ROTATE")
@@ -85,8 +91,7 @@ case "$selected" in
         monitor_rotate
         touch "$ROTATED_FLAG"
     fi
-
-    if niri msg outputs | grep -q "$EXTERNAL"; then
+    if niri msg outputs | grep -qE "($EXT_HDMI|$EXT_DP)"; then
         sleep 1
         monitor_toggle_cmd "$INTERNAL" "$COMPOSITOR" off
     fi
