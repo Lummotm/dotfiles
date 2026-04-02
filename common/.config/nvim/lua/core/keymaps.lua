ac998b99bfcf7735b5f0c2368659ee582326cdde
David Nikolov <Lummotm@proton.me>
2026-04-02 12:09:32 +0200

    (feat): shouldn't use windows managment via nvim, the tmux resize utility I made is better

--- common/.config/nvim/lua/core/keymaps.lua
+++ common/.config/nvim/lua/core/keymaps.lua
@@ -36,25 +36,6 @@ keymap("n", "<leader>ba", ':%bdelete|edit #|normal `"<CR>', { desc = "Delete all
 keymap("n", "<leader>bD", ":bdelete!<CR>", { desc = "Force delete buffer" })
 keymap("n", "<leader>bw", ":w!<CR>", { desc = "BufferWrite" })
 
--- WINDOW MANAGEMENT
--- Create splits
-keymap("n", "<leader>wv", ":split<CR>", { desc = "Split window vertically" })
-keymap("n", "<leader>wh", ":vsplit<CR>", { desc = "Split window horizontally" })
-
--- Close windows
-keymap("n", "<leader>wc", ":close<CR>", { desc = "Close current window" })
-keymap("n", "<leader>wo", ":only<CR>", { desc = "Close all other windows" })
-
--- Resize windows
-keymap("n", "<leader>w=", "<C-w>=", { desc = "Make windows equal size" })
-keymap("n", "<leader>w-", ":resize -2<CR>", { desc = "Decrease window height" })
-keymap("n", "<leader>w+", ":resize +2<CR>", { desc = "Increase window height" })
-keymap("n", "<leader>w<", ":vertical resize -2<CR>", { desc = "Decrease window width" })
-keymap("n", "<leader>w>", ":vertical resize +2<CR>", { desc = "Increase window width" })
-
--- Focus windows (complementa tus tmux mappings)
-keymap("n", "<leader>wf", "<C-w>w", { desc = "Focus next window" })
-
 -- CODE ACTIONS UNIFIED
 -- Auto-indent entire file
 keymap("n", "<leader>ci", "gg=G", { desc = "Auto-indent entire file" })
@@ -86,20 +67,20 @@ end, { desc = "Toggle auto-format on save" })
 
 -- SAGE / MOLTEN (Optimizado)
 -- Gestión del Kernel
-keymap("n", "<leader>mi", ":MoltenInit sagemath<CR>", { desc = "Sage: Init Kernel" })
-
--- Ejecución de Código
-keymap("n", "<leader>me", ":MoltenEvaluateOperator<CR>", { desc = "Sage: Run operator" })
-keymap("n", "<leader>ml", ":MoltenEvaluateLine<CR>", { desc = "Sage: Eval line" })
-keymap("n", "<leader>mr", ":MoltenReevaluateCell<CR>", { desc = "Sage: Re-eval cell" })
-keymap("v", "<leader>me", ":<C-u>MoltenEvaluateVisual<CR>gv", { desc = "Sage: Eval selection" })
-keymap("n", "<leader>ma", "mzggVG:<C-u>MoltenEvaluateVisual<CR>`z", { desc = "Sage: Eval all file" })
-
--- Ventana de Output (Resultados)
-keymap("n", "<leader>mo", ":MoltenEnterOutput<CR>", { desc = "Sage: Open/Enter output" })
-keymap("n", "<leader>mh", ":MoltenHideOutput<CR>", { desc = "Sage: Hide output" })
-keymap("n", "<leader>md", ":MoltenDelete<CR>", { desc = "Sage: Delete cell" })
-
--- Navegación entre celdas (Resultados previos/siguientes)
-keymap("n", "<leader>mj", ":MoltenNext<CR>", { desc = "Sage: Next cell" })
-keymap("n", "<leader>mk", ":MoltenPrev<CR>", { desc = "Sage: Prev cell" })
+-- keymap("n", "<leader>mi", ":MoltenInit sagemath<CR>", { desc = "Sage: Init Kernel" })
+--
+-- -- Ejecución de Código
+-- keymap("n", "<leader>me", ":MoltenEvaluateOperator<CR>", { desc = "Sage: Run operator" })
+-- keymap("n", "<leader>ml", ":MoltenEvaluateLine<CR>", { desc = "Sage: Eval line" })
+-- keymap("n", "<leader>mr", ":MoltenReevaluateCell<CR>", { desc = "Sage: Re-eval cell" })
+-- keymap("v", "<leader>me", ":<C-u>MoltenEvaluateVisual<CR>gv", { desc = "Sage: Eval selection" })
+-- keymap("n", "<leader>ma", "mzggVG:<C-u>MoltenEvaluateVisual<CR>`z", { desc = "Sage: Eval all file" })
+--
+-- -- Ventana de Output (Resultados)
+-- keymap("n", "<leader>mo", ":MoltenEnterOutput<CR>", { desc = "Sage: Open/Enter output" })
+-- keymap("n", "<leader>mh", ":MoltenHideOutput<CR>", { desc = "Sage: Hide output" })
+-- keymap("n", "<leader>md", ":MoltenDelete<CR>", { desc = "Sage: Delete cell" })
+--
+-- -- Navegación entre celdas (Resultados previos/siguientes)
+-- keymap("n", "<leader>mj", ":MoltenNext<CR>", { desc = "Sage: Next cell" })
+-- keymap("n", "<leader>mk", ":MoltenPrev<CR>", { desc = "Sage: Prev cell" })
