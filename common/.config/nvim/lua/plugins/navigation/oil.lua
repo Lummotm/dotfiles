d2d8790192f08a487148eae5bc17324ddc0ac388
David Nikolov <Lummotm@proton.me>
2026-07-30 00:44:31 +0200

    feat: readed oil.nvim, its kinda cool

---
+++ common/.config/nvim/lua/plugins/navigation/oil.lua
@@ -0,0 +1,35 @@
+-- Oil: inbuffer file manager
+return {
+	"stevearc/oil.nvim",
+	lazy = false,
+	keys = { { "<leader>ef", "<cmd>Oil<CR>", desc = "File Explorer (Oil)" } },
+	config = function()
+		require("oil").setup({
+			skip_confirm_for_simple_edits = true,
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+				-- This function defines what is considered a "hidden" file
+				is_hidden_file = function(name, bufnr)
+					local m = name:match("^%.")
+					return m ~= nil
+				end,
+				-- This function defines what will never be shown, even when `show_hidden` is set
+				is_always_hidden = function(name, bufnr)
+					return false
+				end,
+				-- Sort file names with numbers in a more intuitive order for humans.
+				-- Can be "fast", true, or false. "fast" will turn it off for large directories.
+				natural_order = "fast",
+				-- Sort file and directory names case insensitive
+				case_insensitive = false,
+				sort = {
+					-- sort order can be "asc" or "desc"
+					-- see :help oil-columns to see which columns are sortable
+					{ "type", "asc" },
+					{ "name", "asc" },
+				},
+			},
+		})
+	end,
+}
