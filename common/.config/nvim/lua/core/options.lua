48d79ad03ca9cfedcdecac75764831f604a0fafe
David Nikolov <Lummotm@proton.me>
2026-07-31 14:15:54 +0200

    using 2 spaces

--- common/.config/nvim/lua/core/options.lua
+++ common/.config/nvim/lua/core/options.lua
@@ -23,9 +23,9 @@ vim.opt.wrap = true
 vim.opt.conceallevel = 1
 
 -- Indentation: 4 spaces per tab, expand tabs to spaces
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
 -- Wrap indent: Maintain indent level on wrapped lines
