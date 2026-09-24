5cc996ccca4ec832d650bff3358a833d1d8eae57
David Nikolov <Lummotm@proton.me>
2026-09-24 16:31:45 +0200

    idk

--- common/bin/pickers/sioyek-document-opener.sh
+++ common/bin/pickers/sioyek-document-opener.sh
@@ -32,20 +32,42 @@ else
 fi
 
 DOCS=$(
-  fd -L -e pdf -e epub . "${SEARCH_DIRS[@]}" 2>/dev/null | awk -F'/' '{
+  fd -L -e pdf -e epub . "${SEARCH_DIRS[@]}" 2>/dev/null | awk -v search_dirs="${SEARCH_DIRS[*]}" -F'/' '{
         full_path = $0
         filename = $NF
-        parent_dir = $(NF-1)
-        chars = 35
+        
+        # Identificamos la ruta base correspondiente y la recortamos
+        n_dirs = split(search_dirs, dirs, " ")
+        rel_path = full_path
+        
+        for (i=1; i<=n_dirs; i++) {
+            d = dirs[i]
+            gsub(/\/+$/, "", d)
+            if (index(full_path, d "/") == 1) {
+                rel_path = substr(full_path, length(d) + 2)
+                break
+            }
+        }
 
+        # Construimos la ruta de carpetas relativa excluyendo el archivo final
+        n_parts = split(rel_path, parts, "/")
+        dir_path = ""
+        for (j=1; j<n_parts; j++) {
+            dir_path = (j==1) ? parts[j] : dir_path "/" parts[j]
+        }
+
+        chars = 35
         if (length(filename) > chars) {
             short_name = substr(filename, 1, chars) "…"
         } else {
             short_name = filename
         }
 
+        # Si el archivo está directamente en la raíz de la búsqueda
+        if (dir_path == "") dir_path = "."
+
         # Guardamos la ruta completa (full_path) en el delimitador '///'
-        print short_name "\t " parent_dir "\t                                        \t///" full_path
+        print short_name "\t " dir_path "\t                                        \t///" full_path
     }' | column -t -s $'\t'
 )
 
